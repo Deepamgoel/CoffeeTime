8b8c44b2db9486208a77144b4d156f50189b1973
Deepam Goel <deepamgoel4@gmail.com>
2018-03-10 03:04:02 +0530

    Fourth Commit Added gifs, Intent, Delay method

--- app/src/main/java/com/example/deepamgoel/coffeetime/Order.java
+++ app/src/main/java/com/example/deepamgoel/coffeetime/Order.java
@@ -1,18 +1,25 @@
 package com.example.deepamgoel.coffeetime;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.text.NumberFormat;
 import java.util.Locale;
 
 
 public class Order extends AppCompatActivity {
 
 
-    int noOfCoffees = 0;
+    String message;
+    private int noOfCoffees = 1, pricePerCoffee = 30, totalPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,34 +28,191 @@ public class Order extends AppCompatActivity {
     }
 
     public void submitOrder(View view) {
-        displayPrice(noOfCoffees * 10);
+        EditText name = findViewById(R.id.name);
+        Button orderSummary = findViewById(R.id.order_summary);
+        Button confirm = findViewById(R.id.confirm);
+
+        confirm.setVisibility(View.VISIBLE);
+        name.setVisibility(View.GONE);
+        orderSummary.setVisibility(View.INVISIBLE);
+
+        displaySummary(noOfCoffees);
     }
 
     public void inc(View View) {
+
+        //      Name
+        EditText name = findViewById(R.id.name);
+
+        //      Buttons
+        Button orderSummary = findViewById(R.id.order_summary);
+        Button confirm = findViewById(R.id.confirm);
+
+        if (noOfCoffees >= 15) {
+            noOfCoffees = 14;
+            Toast.makeText(this, "Maximum Order: 2 Cups", Toast.LENGTH_SHORT).show();
+        }
+
+        confirm.setVisibility(android.view.View.INVISIBLE);
+        name.setVisibility(android.view.View.VISIBLE);
+        orderSummary.setVisibility(android.view.View.VISIBLE);
         display(++noOfCoffees);
     }
 
     public void dec(View View) {
-        if (noOfCoffees == 0)
-            noOfCoffees++;
+
+        //      Name
+        EditText name = findViewById(R.id.name);
+
+        //      Buttons
+        Button orderSummary = findViewById(R.id.order_summary);
+        Button confirm = findViewById(R.id.confirm);
+
+        if (noOfCoffees <= 1) {
+            noOfCoffees = 2;
+            Toast.makeText(this, "Minimum Order: 1 Cup", Toast.LENGTH_SHORT).show();
+        }
+
+        confirm.setVisibility(android.view.View.INVISIBLE);
+        name.setVisibility(android.view.View.VISIBLE);
+        orderSummary.setVisibility(android.view.View.VISIBLE);
         display(--noOfCoffees);
     }
 
-    /**
-     * This method displays the given quantity value on the screen.
-     */
-    private void display(int number) {
-        TextView quantityTextView = findViewById(R.id.qty);
-        quantityTextView.setText(String.format(Locale.getDefault(), "%d", number));
+
+    public void whipped(View view) {
+
+        //      Name
+        EditText name = findViewById(R.id.name);
+
+        //      Buttons
+        Button orderSummary = findViewById(R.id.order_summary);
+        Button confirm = findViewById(R.id.confirm);
+
+        confirm.setVisibility(android.view.View.INVISIBLE);
+        name.setVisibility(android.view.View.VISIBLE);
+        orderSummary.setVisibility(android.view.View.VISIBLE);
+        display(noOfCoffees);
     }
 
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = findViewById(R.id.price);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+
+    public void chocolate(View view) {
+
+
+        //      Name
+        EditText name = findViewById(R.id.name);
+
+        //      Buttons
+        Button orderSummary = findViewById(R.id.order_summary);
+        Button confirm = findViewById(R.id.confirm);
+
+        confirm.setVisibility(android.view.View.INVISIBLE);
+        name.setVisibility(android.view.View.VISIBLE);
+        orderSummary.setVisibility(android.view.View.VISIBLE);
+        display(noOfCoffees);
     }
+
+
+    private void display(int noOfCoffees) {
+
+        //      TextViews
+        TextView priceSummaryTextView = findViewById(R.id.price);
+        TextView priceText = findViewById(R.id.priceText);
+        TextView quantity = findViewById(R.id.qty);
+
+        //      CheckBox
+        CheckBox whippedCream = findViewById(R.id.whipped_cream);
+        CheckBox chocolate = findViewById(R.id.chocolate);
+
+
+        priceText.setText(R.string.priceText);
+
+        quantity.setText(String.format(Locale.getDefault(), "%d", noOfCoffees));
+
+        totalPrice = noOfCoffees * pricePerCoffee;
+
+        if (whippedCream.isChecked()) {
+            totalPrice += (15 * noOfCoffees);
+        }
+
+        if (chocolate.isChecked()) {
+            totalPrice += (15 * noOfCoffees);
+        }
+
+        priceSummaryTextView.setText(String.format(Locale.getDefault(), "₹%d", totalPrice));
+    }
+
+
+    private void displaySummary(int noOfCoffees) {
+
+
+        //      Name
+        EditText name = findViewById(R.id.name);
+
+        //      TextViews
+        TextView priceSummaryTextView = findViewById(R.id.price);
+        TextView priceText = findViewById(R.id.priceText);
+
+        //      CheckBox
+        CheckBox whippedCream = findViewById(R.id.whipped_cream);
+        CheckBox chocolate = findViewById(R.id.chocolate);
+
+
+        totalPrice = noOfCoffees * pricePerCoffee;
+
+        message = "Name: " + name.getText().toString();
+        message += "\nQuantity: " + noOfCoffees;
+        message += "\nWhipped Cream: ";
+
+        if (whippedCream.isChecked()) {
+            totalPrice += (15 * noOfCoffees);
+            message += "Yes";
+        } else
+            message += "No";
+
+        message += "\nChocolate:";
+
+        if (chocolate.isChecked()) {
+            totalPrice += (15 * noOfCoffees);
+            message += "Yes";
+        } else
+            message += "No";
+
+
+        message += "\nPrice: ₹" + totalPrice;
+        message += "\nThank You";
+
+        priceText.setText(R.string.summary);
+        priceSummaryTextView.setTextSize(30);
+        priceSummaryTextView.setText(message);
+    }
+
+    public void confirm(View view) {
+
+        Handler delay = new Handler();
+        delay.postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+
+                Intent send = new Intent(Intent.ACTION_SENDTO);
+                send.setData(Uri.parse("mailto:"));
+                send.putExtra(Intent.EXTRA_TEXT, message);
+                send.putExtra(Intent.EXTRA_EMAIL, "deepamgoel4@gmail.com");
+                send.putExtra(Intent.EXTRA_SUBJECT, "Coffee Order");
+                startActivity(send);
+                finish();
+            }
+
+        }, 2500);
+
+        pl.droidsonroids.gif.GifImageView yess = findViewById(R.id.yess);
+        yess.setVisibility(View.VISIBLE);
+
+    }
+
+
 
 
 }
